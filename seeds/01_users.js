d41d27723a8fbbd8f61ef9623663446bc1c1276d
Luke Tyler <luketyler17@gmail.com>
2022-09-29 15:44:18 -0400

    removed package.json stuff

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -6,9 +6,9 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    {id: 1, FirstName: 'Luke', LastName: 'Tyler', Username: 'lbtyler', PasswordHash: "admin123"},
-    {id: 2, FirstName: 'Emily', LastName: 'Wilder', Username: 'emwilde', PasswordHash: "admin1234"},
-    {id: 3, FirstName: 'Johnathan', LastName: 'Smith', Username: 'jbsmit', PasswordHash: "admin12345"}
+    {FirstName: 'Luke', LastName: 'Tyler', Username: 'lbtyler', PasswordHash: "admin123"},
+    {FirstName: 'Emily', LastName: 'Wilder', Username: 'emwilde', PasswordHash: "admin1234"},
+    {FirstName: 'Johnathan', LastName: 'Smith', Username: 'jbsmit', PasswordHash: "admin12345"}
   ]);
 };
 
