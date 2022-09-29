cf489e8b4f1306b46860347bc0e76bf423f4fafc
Luke Tyler <luketyler17@gmail.com>
2022-09-29 16:08:08 -0400

    help

--- seeds/02_items.js
+++ seeds/02_items.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('item').del()
   await knex('item').insert([
-    {id: 1, UserId: 2, ItemName: 'M16 Rifles', Description: 'M16 Fully automatic assult weapons, using 5.52mm ammunition', Quantity: 55},
-    {id: 2, UserId: 2, ItemName: 'Kevlar Vest', Description: 'Body protection equiptment', Quantity: 25},
-    {id: 3, UserId: 2, ItemName: 'MRE', Description: 'Meals Ready to eat to provide food for troops', Quantity: 155},
+    {UserId: 2, ItemName: 'M16 Rifles', Description: 'M16 Fully automatic assult weapons, using 5.52mm ammunition', Quantity: 55},
+    {UserId: 2, ItemName: 'Kevlar Vest', Description: 'Body protection equiptment', Quantity: 25},
+    {UserId: 2, ItemName: 'MRE', Description: 'Meals Ready to eat to provide food for troops', Quantity: 155},
   ]);
 };
