c63acf5e84836cafee3e98f59d21a29e119588fd
Luke Tyler <luketyler17@gmail.com>
2022-09-29 13:28:06 -0400

    removed package.json stuff

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,36 @@
 // Update with your config settings.
 require("dotenv").config();
- let connectionString = process.env.DATABASE_URL
+
+let connectionString = process.env.DATABASE_URL;
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
 //he changed dev connection to connectionString also
 module.exports = {
-
   development: {
-    client: 'postgresql',
-    connection: {
-      host: '127.0.0.1',
-      password: 'docker',
-      user: 'postgres',
-      port: 5432,
-      database: 'capstone'
-      }
+    client: "pg",
+    connection: connectionString,
   },
 
   staging: {
-    client: 'postgresql',
+    client: "pg",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'pg',
-    connection: { connectionString, ssl: {rejectUnauthorized: false} }
-  }
-
+    client: "pg",
+    connection: { connectionString, ssl: { rejectUnauthorized: false } },
+  },
 };
